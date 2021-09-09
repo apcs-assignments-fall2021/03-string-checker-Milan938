a0e7246b03505978ffdd21a5b8400b9040e7f1d3
milanboga <23mboga@athenian.org>
2021-09-09 11:37:30 -0700

    Still Working

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -1,23 +1,26 @@
 import java.util.Scanner;
 
 public class MyMain {
-    
+
     // Counts the number of 'a', 'b', and 'c' chars in str
     public static int countABC(String str) {
-        int a = str.indexOf("a");
-        int b = str.indexOf("b");
-        int c = str.indexOf("c");
-        int total = 0;
-        if (a > -1) {
-            total = total +1;
+        for (int i = 0; i < str.length(); i++) {
+            System.out.print(str.charAt(i));
+            int a_count = 0;
+            int b_count = 0;
+            int c_count = 0;
+            if (str.charAt(i) == ('a')) {
+                a_count = a_count + 1;
+            }
+            if (str.charAt(i) == ('b')) {
+                b_count = b_count + 1;
+            }
+            if (str.charAt(i) == ('c')) {
+                c_count = c_count + 1;
+            }
+            System.out.print("There are " + a_count + b_count + c_count + " a's, b's, or c's");
+            return -1;
         }
-        if (b > -1) {
-            total = total +1;
-        }
-        if (c > -1) {
-            total = total +1;
-        }
-        System.out.print("There are " + total + " a's, b's, or c's");
         return -1;
     }
 
@@ -30,22 +33,28 @@ public class MyMain {
         }
         if (b > -1) {
             return true;
-        }
-        else {
+        } else {
             return false;
         }
     }
 
-    // Checks whether str is a palindrome or not
+    //Checks whether str is a palindrome or not
     public static boolean isPalindrome(String str) {
-        // REPLACE WITH YOUR CODEE
-        return false;
+        int length = str.length() - 1;
+        for (int i = 0; i <= str.length(); i++) {
+
+        }
     }
-    
-    
+
+
+
+
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-
-        // YOUR CODE HERE
+        System.out.print(countABC("hamster"));
     }
 }
+
+        // YOUR CODE HERE
+
+
