1119cbece60a89fc8c8c7ec972189fb94d590c4c
milanboga <23mboga@athenian.org>
2021-09-08 20:45:43 -0700

    Not finished, not sure how to do or check code, will come to C and C tomorrow

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -4,14 +4,36 @@ public class MyMain {
     
     // Counts the number of 'a', 'b', and 'c' chars in str
     public static int countABC(String str) {
-        // REPLACE WITH YOUR CODE
+        int a = str.indexOf("a");
+        int b = str.indexOf("b");
+        int c = str.indexOf("c");
+        int total = 0;
+        if (a > -1) {
+            total = total +1;
+        }
+        if (b > -1) {
+            total = total +1;
+        }
+        if (c > -1) {
+            total = total +1;
+        }
+        System.out.print("There are " + total + " a's, b's, or c's");
         return -1;
     }
 
     // Checks to see whether or not str contains 'The' or 'the'
     public static boolean containsThe(String str) {
-        // REPLACE WITH YOUR CODE
-        return false;
+        int a = str.indexOf("The");
+        int b = str.indexOf("the");
+        if (a > -1) {
+            return true;
+        }
+        if (b > -1) {
+            return true;
+        }
+        else {
+            return false;
+        }
     }
 
     // Checks whether str is a palindrome or not
